102cfa01a881b43f99b0d3ab9cab2fd13de7a476
rmbinder <r-m-b@gmx.net>
2022-01-10 13:09:38 +0100

    update version to 2.1.0

--- declaration_of_membership.php
+++ declaration_of_membership.php
@@ -3,14 +3,14 @@
  ***********************************************************************************************
  * DeclarationOfMembership (ONLINE-Beitrittserklärung)
  *
- * Version 2.0.0
+ * Version 2.1.0
  *
  * This plugin creates an online - declaration of membership.
  * 
  *
  * Author: rmb
  *
- * Compatible with Admidio version 4
+ * Compatible with Admidio version 4.1
  *
  * @copyright 2004-2022 The Admidio Team
  * @see https://www.admidio.org/
