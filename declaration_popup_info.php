3828c5f1cf288c53c275a0b9c977b956feb32f8b
rmbinder <r-m-b@gmx.net>
2023-02-09 18:36:35 +0100

    new year 2023

--- declaration_popup_info.php
+++ declaration_popup_info.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * Erzeugt ein Modal-Fenster mit Plugininformationen
  *
- * @copyright 2004-2022 The Admidio Team
+ * @copyright 2004-2023 The Admidio Team
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
  */
